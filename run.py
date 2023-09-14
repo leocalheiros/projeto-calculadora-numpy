8575d9f8360418a5be08565eda18d5f2bd740c21
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-14 10:46:55 -0300

    feat: add run.py to project

---
+++ run.py
@@ -0,0 +1,10 @@
+from src.main.process_handler import ProcessHandler
+
+
+def main():
+    process_handler = ProcessHandler()
+    process_handler.start()
+
+
+if __name__ == "__main__":
+    main()
