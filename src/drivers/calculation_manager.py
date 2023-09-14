695361f42f103192f4fdc3e3fb50c06b81c3bdf8
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-14 08:02:15 -0300

    feat: add calculation_manager.py driver for numpy

---
+++ src/drivers/calculation_manager.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class CalculationManager:
+    def __init__(self) -> None:
+        self.__np = np
+
+    def average(self, values):
+        return self.__np.average(values)
+
+    def variance(self, values):
+        return self.__np.variance(values)
+
+    def std_deviation(self, values):
+        return self.__np.std_deviation(values)
