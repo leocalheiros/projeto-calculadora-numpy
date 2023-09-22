88c9b6789aae05933694703c93f71e8a40a1a134
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-22 14:54:29 -0300

    feat: add routes logic and second calculator controller new logic

--- src/controllers/second_calculator_controller.py
+++ src/controllers/second_calculator_controller.py
@@ -1,21 +1,58 @@
 from src.drivers.calculation_manager import CalculationManager
 from .interface.calculator_interface import ICalculator
+from src.main.http_types.http_response import HttpResponse
+from src.main.http_types.http_request import HttpRequest
 
 
 class SecondCalculator(ICalculator):
     def __init__(self, calculation_manager: CalculationManager) -> None:
         self.__calculation_manager = calculation_manager
 
-    def calculate(self, values):
-        powered_values = [(value * 11) ** 0.95 for value in values]
+    def __calculate(self, values):
+        print(f"Received values: {values}")
 
-        std_deviation = self.__calculation_manager.std_deviation(powered_values)
+        powered_values = []
 
-        if std_deviation == 0:
+        if not isinstance(values, list):
+            values = [values]
+
+        for value in values:
+            if value is not None:
+                powered_value = (value * 11) ** 0.95
+                print(f"Powered value: {powered_value}")
+                powered_values.append(powered_value)
+
+        if not powered_values:
             result = None
             status = "Falha"
         else:
-            result = 1 / std_deviation
-            status = "Sucesso"
+            std_deviation = self.__calculation_manager.std_deviation(powered_values)
+            if std_deviation == 0:
+                result = None
+                status = "Falha"
+                print('Estou dando erro aqui')
+                print(powered_values)
+            else:
+                result = 1 / std_deviation
+                status = "Sucesso"
+            print(f"Result: {result}, Status: {status}")
 
-        return result, status
+        return {
+            "status": status,
+            "result": result
+        }
+
+    def execute(self, req: HttpRequest) -> HttpResponse:
+        values = req.body.get('value')
+        result_dict = self.__calculate(values)
+
+        status = result_dict["status"]
+        result = result_dict["result"]
+
+        return HttpResponse(200, {
+            "calculator": "Second Calculator",
+            "inputs": [values],
+            "status": status,
+            "result": result
+        })
+
--- src/main/server/routes.py
+++ src/main/server/routes.py
@@ -1,7 +1,51 @@
 from flask import request, jsonify
+from src.controllers.first_calculator_controller import FirstCalculator
+from src.controllers.second_calculator_controller import SecondCalculator
+from src.controllers.third_calculator_controller import ThirdCalculator
+from src.main.http_types.http_request import HttpRequest
+from src.drivers.calculation_manager import CalculationManager
 from .server import app
 
+calculation_manager = CalculationManager()
 
-@app.route("/", methods="POST")
+
+@app.route("/calculate", methods=["POST"])
 def calculator_function():
-    pass
+    request_data = request.json
+
+    if 'calculator_type' not in request_data or 'values' not in request_data:
+        return jsonify({'error': 'Campos obrigatórios ausentes'}, 400)
+
+    calculator_type = request_data.get('calculator_type')
+    values = request_data.get('values')
+
+    if calculator_type == 'first':
+        calculator = FirstCalculator(calculation_manager)
+        calculator_name = 'First Calculator'
+    elif calculator_type == 'second':
+        calculator = SecondCalculator(calculation_manager)
+        calculator_name = 'Second Calculator'
+    elif calculator_type == 'third':
+        calculator = ThirdCalculator(calculation_manager)
+        calculator_name = 'Third Calculator'
+    else:
+        return jsonify({'error': 'Tipo de calculadora não suportado'}, 400)
+
+    results = []
+    for value in values:
+        req = HttpRequest(
+            header=request.headers,
+            body={"value": value},
+        )
+
+        response = calculator.execute(req)
+
+        result_dict = {
+            "calculator": calculator_name,
+            "inputs": [value],
+            "status": response.status_code,
+            "result": response.body
+        }
+        results.append(result_dict)
+
+    return jsonify(results), 200
