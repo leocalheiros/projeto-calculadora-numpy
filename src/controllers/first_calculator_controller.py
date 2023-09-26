650ddf84923fd634528d746e17b03898258d444c
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-26 13:26:15 -0300

    fix: refactoring first controller to works with http requests

--- src/controllers/first_calculator_controller.py
+++ src/controllers/first_calculator_controller.py
@@ -9,13 +9,9 @@ class FirstCalculator(ICalculator):
         self.__calculation_manager = calculation_manager
 
     def __calculate(self, value):
-        part1 = value / 3
-        part2 = value / 3
-        part3 = value / 3
-
-        part1 = (part1 / 4 + 7) ** 0.5 * 0.257
-
-        part2 = (part2 ** 2.121) / 5 + 1
+        part1 = self.__calculate_part1(value)
+        part2 = self.__calculate_part2(value)
+        part3 = self.__calculate_part3(value)
 
         result = self.__calculation_manager.average([part1, part2, part3])
         result = round(result, 1)
@@ -27,6 +23,19 @@ class FirstCalculator(ICalculator):
             "result": result
         }
 
+    def __calculate_part1(self, value):
+        part1 = value / 3
+        part1 = (part1 / 4 + 7) ** 0.5 * 0.257
+        return part1
+
+    def __calculate_part2(self, value):
+        part2 = value / 3
+        part2 = (part2 ** 2.121) / 5 + 1
+        return part2
+
+    def __calculate_part3(self, value):
+        return value / 3
+
     def execute(self, req: HttpRequest) -> HttpResponse:
         value = req.body.get('value')
         result_dict = self.__calculate(value)
@@ -35,7 +44,6 @@ class FirstCalculator(ICalculator):
         result = result_dict["result"]
 
         return HttpResponse(200, {
-            "calculator": "First Calculator",
             "inputs": [value],
             "status": status,
             "result": result
