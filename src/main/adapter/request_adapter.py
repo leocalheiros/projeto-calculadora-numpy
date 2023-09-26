74563fceca290890c28a6c4bf86d958919a1e1b5
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-26 13:25:33 -0300

    fix: refactoring request_adapter.py with single http_response

--- src/main/adapter/request_adapter.py
+++ src/main/adapter/request_adapter.py
@@ -1,17 +1,8 @@
-from flask import request as FlaskRequest
 from src.controllers.interface.calculator_interface import ICalculator
 from src.main.http_types.http_response import HttpResponse
 from src.main.http_types.http_request import HttpRequest
 
 
-def request_adapter(request: FlaskRequest, calculator: ICalculator) -> HttpResponse:
-    req = HttpRequest(
-        header=dict(request.headers),
-        body=request.json,
-        query_params=dict(request.args),
-        path_params=request.view_args,
-        url=request.full_path,
-        ipv4=request.remote_addr,
-    )
-    http_response = calculator.execute(req)
+def request_adapter(request: HttpRequest, calculator: ICalculator) -> HttpResponse:
+    http_response = calculator.execute(request)
     return http_response
