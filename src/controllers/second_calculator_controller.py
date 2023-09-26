d02bac224a62e6d69df9cc6eca324c72eacd33bf
Leonardo Calheiros <leonardo.calheiros@gmail.com>
2023-09-26 13:27:12 -0300

    fix: refactoring second controller to works with http requests

--- src/controllers/second_calculator_controller.py
+++ src/controllers/second_calculator_controller.py
@@ -1,7 +1,7 @@
-from src.drivers.calculation_manager import CalculationManager
-from .interface.calculator_interface import ICalculator
 from src.main.http_types.http_response import HttpResponse
 from src.main.http_types.http_request import HttpRequest
+from src.drivers.calculation_manager import CalculationManager
+from .interface.calculator_interface import ICalculator
 
 
 class SecondCalculator(ICalculator):
@@ -9,50 +9,31 @@ class SecondCalculator(ICalculator):
         self.__calculation_manager = calculation_manager
 
     def __calculate(self, values):
-        print(f"Received values: {values}")
+        powered_values = [(value * 11) ** 0.95 for value in values]
+        return powered_values
 
-        powered_values = []
+    def __calculate_std_deviation(self, powered_values):
+        std_deviation = self.__calculation_manager.std_deviation(powered_values)
+        return std_deviation
 
-        if not isinstance(values, list):
-            values = [values]
-
-        for value in values:
-            if value is not None:
-                powered_value = (value * 11) ** 0.95
-                print(f"Powered value: {powered_value}")
-                powered_values.append(powered_value)
-
-        if not powered_values:
+    def __calculate_result(self, std_deviation):
+        if std_deviation == 0:
             result = None
             status = "Falha"
         else:
-            std_deviation = self.__calculation_manager.std_deviation(powered_values)
-            if std_deviation == 0:
-                result = None
-                status = "Falha"
-                print('Estou dando erro aqui')
-                print(powered_values)
-            else:
-                result = 1 / std_deviation
-                status = "Sucesso"
-            print(f"Result: {result}, Status: {status}")
-
-        return {
-            "status": status,
-            "result": result
-        }
+            result = 1 / std_deviation
+            status = "Sucesso"
+        return result, status
 
     def execute(self, req: HttpRequest) -> HttpResponse:
-        values = req.body.get('value')
-        result_dict = self.__calculate(values)
+        values = req.body.get('values', [])
 
-        status = result_dict["status"]
-        result = result_dict["result"]
+        powered_values = self.__calculate(values)
+        std_deviation = self.__calculate_std_deviation(powered_values)
+        result, status = self.__calculate_result(std_deviation)
 
         return HttpResponse(200, {
-            "calculator": "Second Calculator",
-            "inputs": [values],
+            "inputs": values,
             "status": status,
             "result": result
         })
-
